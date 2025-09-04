da8bc4b27595e173be880af68a372e0d7fd64920
Kenenisa Alemayehu <53818162+kenenisa@users.noreply.github.com>
2025-09-04 22:20:28 +0300

    Initialize gh-pages branch

--- ora/Services/Haptic.swift
+++
@@ -1,6 +0,0 @@
-import SwiftUI
-
-func performHapticFeedback(pattern: NSHapticFeedbackManager.FeedbackPattern) {
-    let manager = NSHapticFeedbackManager.defaultPerformer
-    manager.perform(pattern, performanceTime: .drawCompleted)
-}
