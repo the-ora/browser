bd9983e126c9c80c1faddc9cb10e467bdb092961
ramaID <rama@qisthi.dev>
2025-09-08 22:52:56 +0700

    wip

--- xcbuild-debug copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -o pipefail && xcodebuild build \
-  -target ora \
-  -project Ora.xcodeproj \
-  -destination "platform=macOS" \
-  -configuration Debug \
-  CODE_SIGN_IDENTITY="" \
-  CODE_SIGNING_REQUIRED=NO | xcbeautify
