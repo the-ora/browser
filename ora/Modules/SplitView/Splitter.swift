1d248b7ad53d9420ab90e0f293425ceecabca907
yonaries <yonatandejene001@gmail.com>
2025-09-13 23:03:27 +0300

    reduce splitter invisible width

--- ora/Modules/SplitView/Splitter.swift
+++ ora/Modules/SplitView/Splitter.swift
@@ -41,7 +41,7 @@ public struct Splitter: SplitDivider {
     public static var defaultColor: Color = .gray
     public static var defaultInset: CGFloat = 6
     public static var defaultVisibleThickness: CGFloat = 0
-    public static var defaultInvisibleThickness: CGFloat = 30
+    public static var defaultInvisibleThickness: CGFloat = 16
 
     public var body: some View {
         ZStack {
