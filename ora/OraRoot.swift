aaa8a87c8aaaa7cf25dc1b1c6320688e2c2524bf
Chase <croossin94@gmail.com>
2025-09-28 15:08:10 -0700

    feat: double click full resize of window (#128)
    * feat: wip double click
    
    * feat: add double click to maximize view
    
    * remove deprecation change
    
    * fix: when resizing use previous frame

--- ora/OraRoot.swift
+++ ora/OraRoot.swift
@@ -80,6 +80,7 @@ struct OraRoot: View {
     var body: some View {
         BrowserView()
             .background(WindowReader(window: $window))
+            .environment(\.window, window)
             .environmentObject(appState)
             .environmentObject(tabManager)
             .environmentObject(historyManager)
